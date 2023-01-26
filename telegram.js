955ee4bd38a368ca5feb481aff238a710ba24774
Flmob <flashmob986@gmail.com>
2023-01-26 20:17:17 +0200

    updated save score logic

--- telegram.js
+++ telegram.js
@@ -1,3 +1,7 @@
+const url = new URL(location.href);
+const params = Object.fromEntries(url.searchParams);
+let lastSavedScore = 0;
+
 let loadingIndicator;
 
 const setLoadingState = (isLoading = false) => {
@@ -5,28 +9,39 @@ const setLoadingState = (isLoading = false) => {
   else loadingIndicator.classList.add("hidden");
 };
 
-const url = new URL(location.href);
-const params = Object.fromEntries(url.searchParams);
-
-const updateGameScore = (score) => {
-  const message = `You've lost! Your score is ${score}.`;
-
-  if (!score) return;
-
+const saveScore = (score = 0) => {
   setLoadingState(true);
+
+  if (!score || lastSavedScore > score) {
+    return Promise.resolve().then(() => setLoadingState(false));
+  }
+
   return fetch("/setscore", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ ...params, score }),
   })
     .then((res) => {
-      console.log(message);
+      lastSavedScore = score;
+      setLoadingState(false);
     })
     .catch((err) => {
-      console.log(`${message}\nSorry, couldn't save your new score`);
-    })
-    .finally(() => {
       setLoadingState(false);
+      throw err;
+    });
+};
+
+const updateGameScore = (score) => {
+  if (!score) return;
+
+  const message = `You've lost! Your score is ${score}.`;
+
+  saveScore(score)
+    .then(() => {
+      console.log(message);
+    })
+    .catch(() => {
+      console.log(`${message}\nSorry, couldn't save your new score`);
     });
 };
 
